12ad2fdcca7b7f13413bdc634a3b6c7be70c94b6
Jagjot Singh <singhjagjot63@gmail.com>
2023-10-09 00:17:25 +0530

    ArrayList in java

--- src/main/java/com/collectionsPratice/CollectionsTest.java
+++ src/main/java/com/collectionsPratice/CollectionsTest.java
@@ -1,20 +1,57 @@
 package com.collectionsPratice;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class CollectionsTest {
     public static void main(String[] args) {
-        OurGenericList<Integer> list = new OurGenericList<>();
-        list.add(1);
-        list.add(6);
-        list.add(7);
 
-        for(int i:list) {
-            System.out.println(i);
-        }
+        /*This part of code shows the working of iterator and BTS of "for" loop.*/
+//        OurGenericList<Integer> list = new OurGenericList<>();
+//        list.add(1);
+//        list.add(6);
+//        list.add(7);
+//
+//        for(int i:list) {
+//            System.out.println(i);
+//        }
+//
 //        Iterator<Integer> iterator = list.iterator();
 //        while(iterator.hasNext()) {
 //            System.out.println(iterator.next());
 //        }
+
+//        List<String> listOfNames1 = new ArrayList<>();
+//        /*Integer is a class, and we pass it like List<Integer> = new ArrayList<>();.
+//        * Generics only allow reference types as parameters. Hence, whenever we will
+//        * be making use of generics, reference types have to passed as parameters. */
+//        listOfNames1.add("Jagjot");
+//        listOfNames1.add("Smarth");
+//        listOfNames1.add("Ankur");
+//        listOfNames1.add("Supriyam");
+//
+//        /* This is how one list can be copied to another list.
+//        * Any type of collection can be passed as a parameter when we want to copy. */
+//        List<String> listOfNames2 = new ArrayList<>(listOfNames1);
+//        listOfNames2.add("Harman");
+//        System.out.println(listOfNames1);
+//        System.out.println(listOfNames2);
+//
+//        //Below is the use of method addAll and set
+//        listOfNames2.addAll(listOfNames1);
+//        System.out.println(listOfNames2);
+//        listOfNames2.set(8,"Jagjot");
+//        System.out.println(listOfNames2);
+//
+//        /* In a subList method, a new sublist will be created.
+//        But if any change made to the sublist, the same change
+//        will be reflected to the original list as well*/
+//        List<String> subListOfNames2 = listOfNames2.subList(0,5);
+//        System.out.println(subListOfNames2);
+//        subListOfNames2.set(0,"Jagjot Singh");
+//        System.out.println(subListOfNames2);
+//        System.out.println(listOfNames2);
+
     }
 }
